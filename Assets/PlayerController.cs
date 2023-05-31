3f5b3a13af380327cb1c5f1a372536cff82a6fde
PaulinaHrra <106337486+PaulinaHrra@users.noreply.github.com>
2023-05-31 15:35:11 -0600

    fssfd

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,8 +18,53 @@ public class PlayerController : MonoBehaviour
         // Calcular la dirección del movimiento
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
 
-        // Mover al jugador en la dirección calculada
-        transform.Translate(direction * speed * Time.deltaTime);
+        // Calcular la posición objetivo
+        Vector3 targetPosition = transform.position + direction * speed * Time.deltaTime;
+
+        // Verificar si la posición objetivo está dentro de los límites
+        if (IsPositionWithinBounds(targetPosition))
+        {
+            // Mover al jugador a la posición objetivo
+            transform.position = targetPosition;
+        }
+        else
+        {
+            // Reiniciar el juego si el jugador toca un obstáculo o sale de los límites
+            RestartGame();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            // Reiniciar el juego si el jugador colisiona con un obstáculo
+            RestartGame();
+        }
+    }
+
+    bool IsPositionWithinBounds(Vector3 position)
+    {
+        // Obtener los límites de la escena
+        Bounds bounds = new Bounds(Vector3.zero, new Vector3(10f, 10f, 10f)); // Establece los límites como desees
+
+        // Verificar si la posición está dentro de los límites
+        if (bounds.Contains(position))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void RestartGame()
+    {
+        // Reiniciar el juego cargando la escena actual
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
+
+
 
