00efa5ec8c226ad651c5dd2a3ddd55a196fb9e4c
PaulinaHrra <106337486+PaulinaHrra@users.noreply.github.com>
2023-06-23 22:51:13 -0600

    sdfsdsfdsfsd

---
+++ Pacman_Juego_Parcial/My project/Assets/Scripts/SpawnNodes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNodes : MonoBehaviour
+{
+    int numToSpawn = 28;
+    public float currentSpawnOffset;
+    public float spawnOffset = 0.3f;
+
+     void Start()
+    {
+
+       
+        
+            
+            
+    }
+
+    
+}
